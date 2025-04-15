8f90a1c68f4eec38c656e8eb991d3beecbf15f62
Renan <renan.carli@hotmail.com>
2025-04-15 19:32:45 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fiap/hackathon_vacina/HackathonVacinaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_vacina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonVacinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonVacinaApplication.class, args);
+	}
+
+}
